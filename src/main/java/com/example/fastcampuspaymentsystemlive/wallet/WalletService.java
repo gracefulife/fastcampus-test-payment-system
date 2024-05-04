12fe3640336a66e02690ddcb34023a4a23826779
gracefulife <gracefulife@naver.com>
2024-05-04 23:17:16 +0900

    [3-2] 데이터 무결성 보장하기 with Java

--- src/main/java/com/example/fastcampuspaymentsystemlive/wallet/WalletService.java
+++ src/main/java/com/example/fastcampuspaymentsystemlive/wallet/WalletService.java
@@ -6,6 +6,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -13,6 +15,16 @@ public class WalletService {
     private static final BigDecimal BALANCE_LIMIT = new BigDecimal(100_000);
 
     private final WalletRepository walletRepository;
+
+    private Map<Long, Wallet> walletMap;
+
+    {
+        walletMap = new HashMap<>();
+        Wallet wallet = new Wallet();
+        wallet.setBalance(BigDecimal.ZERO);
+        wallet.setId(1L);
+        walletMap.put(1L, wallet);
+    }
 
     // CRUD
     // Create Read Update Delete
@@ -74,6 +86,35 @@ public class WalletService {
         return new AddBalanceWalletResponse(
                 wallet.getId(), wallet.getUserId(), wallet.getBalance(),
                 wallet.getCreatedAt(), wallet.getUpdatedAt()
+        );
+    }
+
+    public AddBalanceWalletResponse addBalanceJava(AddBalanceWalletRequest request) {
+        final Wallet wallet = walletMap.get(request.walletId());
+        BigDecimal balance = wallet.getBalance();
+        balance = balance.add(request.amount());
+        if (balance.compareTo(BigDecimal.ZERO) < 0) {
+            throw new RuntimeException("잔액이 충분하지 않습니다.");
+        }
+        if (BALANCE_LIMIT.compareTo(balance) < 0) {
+            throw new RuntimeException("한도를 초과했습니다.");
+        }
+
+        wallet.setBalance(balance);
+        wallet.setUpdatedAt(LocalDateTime.now());
+        walletMap.put(wallet.getId(), wallet);
+
+        return new AddBalanceWalletResponse(
+                wallet.getId(), wallet.getUserId(), wallet.getBalance(),
+                wallet.getCreatedAt(), wallet.getUpdatedAt()
+        );
+    }
+
+    public FindWalletResponse findWalletJava(Long id) {
+        Wallet wallet = walletMap.get(id);
+        return new FindWalletResponse(
+                wallet.getId(), wallet.getUserId(), wallet.getBalance(),
+                LocalDateTime.now(), LocalDateTime.now()
         );
     }
 }
