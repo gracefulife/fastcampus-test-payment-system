10e72832185491efca028a6ffe73df78acd733c8
gracefulife <gracefulife@naver.com>
2024-05-04 23:47:04 +0900

    Revert "[3-2] 데이터 무결성 보장하기 with Java"
    This reverts commit 12fe3640336a66e02690ddcb34023a4a23826779.

--- src/test/java/com/example/fastcampuspaymentsystemlive/WalletServiceIntgTest.java
+++ src/test/java/com/example/fastcampuspaymentsystemlive/WalletServiceIntgTest.java
@@ -1,6 +1,9 @@
 package com.example.fastcampuspaymentsystemlive;
 
-import com.example.fastcampuspaymentsystemlive.wallet.*;
+import com.example.fastcampuspaymentsystemlive.wallet.CreateWalletRequest;
+import com.example.fastcampuspaymentsystemlive.wallet.CreateWalletResponse;
+import com.example.fastcampuspaymentsystemlive.wallet.WalletRepository;
+import com.example.fastcampuspaymentsystemlive.wallet.WalletService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -62,36 +64,5 @@ public class WalletServiceIntgTest {
         boolean finished = service.awaitTermination(1, TimeUnit.MINUTES);
         System.out.println(finished);
         System.out.println(walletRepository.findAllByUserId(userId));
-    }
-
-
-    @Test
-    public void 동시에_충전을_진행한다() throws InterruptedException {
-        Long walletId = 1L;
-        AddBalanceWalletRequest request = new AddBalanceWalletRequest(
-                walletId, BigDecimal.TEN);
-
-        int numOfThread = 100;
-        ExecutorService service = Executors.newFixedThreadPool(numOfThread);
-        AtomicInteger completedTasks = new AtomicInteger(0);
-
-        for (int i = 0; i < numOfThread; i++) {
-            service.submit(() -> {
-                try {
-                    walletService.addBalanceJava(request);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    completedTasks.incrementAndGet();
-                }
-            });
-        }
-
-        service.shutdown();
-        boolean finished = service.awaitTermination(1, TimeUnit.MINUTES);
-        System.out.println(finished);
-        System.out.println(walletService.findWalletJava(walletId));
-        Assertions.assertEquals(1000L,
-                walletService.findWalletJava(walletId).balance().longValue());
     }
 }
